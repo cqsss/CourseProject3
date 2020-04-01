64a4d0cb3f59df2eff8dec244e8a82effb6bc435
1306706412 <1306706412@qq.com>
2020-04-01 21:14:01 +0800

    4.1

--- src/main/java/com/cqsss/demo/ZwController.java
+++ src/main/java/com/cqsss/demo/ZwController.java
@@ -4,10 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+class Point{
+        double x;
+        double y;
+        Point(){
+            x=122.09;
+            y=37.54;
+        }
+}
 @Controller
 public class ZwController {
     @RequestMapping("/zw")
-    public String test(){
-        return "zwnb";
+    public String test(Model model){
+        Point p=new Point();
+        model.addAttribute("p_x",p.x);
+        model.addAttribute("p_y",p.y);
+        return "api";
     }
 }
