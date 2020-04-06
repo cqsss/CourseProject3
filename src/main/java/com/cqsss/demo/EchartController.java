a7ad696004cd00b12468f5c2b4036101697030a7
cqsss <870663413@qq.com>
2020-04-06 20:54:05 +0800

    1.0.2 echart

--- src/main/java/com/cqsss/demo/EchartController.java
+++ src/main/java/com/cqsss/demo/EchartController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class EchartController {
     @RequestMapping("/echarttest")
     public String myDemo(){
@@ -12,6 +12,6 @@ public class EchartController {
     }
     @RequestMapping("/calendartest")
     public String myDemo2(){
-        return "custom-calendar-icon";
+        return "calendartest";
     }
 }
