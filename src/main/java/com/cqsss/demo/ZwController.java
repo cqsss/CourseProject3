fb152b4d61d89c9d747502453f91cf0564ded92a
1306706412 <1306706412@qq.com>
2020-04-06 19:52:58 +0800

    zw

--- src/main/java/com/cqsss/demo/ZwController.java
+++ src/main/java/com/cqsss/demo/ZwController.java
@@ -19,6 +19,9 @@ public class ZwController {
         Point p=new Point();
         model.addAttribute("p_x",p.x);
         model.addAttribute("p_y",p.y);
+       model.addAttribute("title","周伟测试");
+       model.addAttribute("address","周伟测试的地址：哈工大周伟测试的寝室");
+
         return "api";
     }
 }
