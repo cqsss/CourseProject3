92e45f1fea21024570ce326f25b676f9e4a9d1df
1306706412 <1306706412@qq.com>
2020-04-08 14:07:51 +0800

    new

--- src/main/java/com/cqsss/demo/inputController.java
+++ src/main/java/com/cqsss/demo/inputController.java
@@ -3,10 +3,17 @@ package com.cqsss.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.time.DateTimeException;
+
 @Controller
 public class inputController {
     @RequestMapping("/input")
-    public String aa() {
+    public String aa(HttpServletRequest request) {
+        String meeting_name=request.getParameter("meetingname");
+        String meeting_date=request.getParameter("meetingdate");
+        System.out.println(meeting_name);
+        System.out.println(meeting_date);
         return "input";
     }
 }
